33584a0a29e5e7039f1a6f9fd5a80ea27a2d70e3
amanraghuvanshi <iamanraghuvanshi@gmail.com>
2025-06-23 14:39:36 +0530

    initializing zoomtool function

--- app.py
+++ app.py
@@ -14,3 +14,22 @@ from datetime import datetime, timedelta
 from agno.models.openai import OpenAIChat
 from streamlit_pdf_viewer import pdf_viewer
 from typing import Literal, Tuple, Dict, Optional
+
+# Class for controlling zoom access
+class CustomZoomTool(ZoomTool):
+    def __init__(self, *, account_id: Optional[str] = None, client_id: Optional[str] = None,
+             client_secret: Optional[str] = None, name: str = "zoom_tool"):
+        super().__init__(account_id=account_id, client_id=client_id, client_secret=client_secret, name=name)
+        self.token_url = "https://zoom.us/oauth/token"
+        self.access_token = None
+        self.token_expires_at = 0
+        
+    def get_access_token(self) -> str:
+        if self.access_token and time.time() < self.token_expires_at:
+            return str(self.access_token)
+            
+        headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        data = {
+            "grant_type": "account_credentials", 
+            "account_id": self.account_id
+            }
